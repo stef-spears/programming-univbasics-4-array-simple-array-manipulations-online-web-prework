180e80711d0641bed43d086bf424700f9afc4fc6
Stef Spears <spears.sj87@gmail.com>
2019-07-09 06:37:53 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_push(Array, String)
+  Array.push(String)
+end
+
+def using_unshift(Array, String)
+  Array.unshift(String)
+end
+
+def using_pop(Array)
+  p Array.pop
+end
+
+def pop_with_args(Array)
+  Array.pop(2)
+end
+
+def using_shift(Array)
+  Array.shift
+end
+
+
+  
